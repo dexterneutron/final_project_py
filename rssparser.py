e9240d0c4e5b6830c16a5dd5abe4e671a728b47e
DESKTOP-2QIG43J\felix.lugo <felixdlugo@gmail.com>
2021-10-01 10:35:07 -0400

    Finished RSS Parsing Class

--- rssparser.py
+++ rssparser.py
@@ -4,19 +4,18 @@ import requests
 VERGE_URL = 'https://www.theverge.com/rss/index.xml'
 TECHCRUNCH_URL = 'https://techcrunch.com/feed/'
 MASHABLE_URL = 'https://mashable.com/feeds/rss/all'
-
-
-headers = {
+HEADERS = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
         }
  
+
 class ReadRss:
     def __init__(self, headers, url_list):
         self.reponse_objects = {}
-        self.urls = {"verge":VERGE_URL,"techcrunch":TECHCRUNCH_URL, "mashable":MASHABLE_URL}
+        urls = {"verge":VERGE_URL,"techcrunch":TECHCRUNCH_URL, "mashable":MASHABLE_URL}
+        
         for page in url_list:
-            rss_url =  self.urls[page]       
-            self.headers = headers
+            rss_url =  urls[page]       
             try:
                 r = requests.get(rss_url, headers = headers)
                 self.reponse_objects[page] = r
@@ -25,7 +24,13 @@ class ReadRss:
                 print(e)
 
     def parse_articles(self):
+        
+        all_articles = []
+
         for key,value in self.reponse_objects.items():
+          
+          #Each RSS site has its own structure, so, it's needed to parse each of them separately
+
             if key == "verge":
                 try:    
                     soup = BeautifulSoup(value.text, 'lxml')
@@ -34,14 +39,16 @@ class ReadRss:
                     print(e)
 
                 articles = soup.findAll('entry')
-                self.articles_dicts = [{'title':a.find('title').getText(),
+                articles_dicts = [{'source':'The Verge',
+                'title':a.find('title').getText(),
                 'content':a.find('content').getText(),
                 'published':a.find('published').getText(),
                 'author':a.find('author').find('name').getText(),
                 'id':a.find('id').getText(),
                 } for a in articles]
+                all_articles.append(articles_dicts)
 
-            if key == "techcrunch":
+            elif key == "techcrunch":
                 try:    
                     soup = BeautifulSoup(value.text, 'xml')
                 except Exception as e:    
@@ -49,17 +56,40 @@ class ReadRss:
                     print(e)
 
                 articles = soup.findAll('item')
-                articles_dicts = [{'title':a.find('title').text,
+                articles_dicts = [{'source':'Techcrunch',
+                'title':a.find('title').text,
                 'content':a.find('content:encoded').text,
                 'published':a.find('pubDate').text,
                 'author':a.find('dc:creator').text,
                 'id':a.find('link').text
                 } for a in articles]
+                all_articles.append(articles_dicts)
 
-        return articles_dicts
+            elif key == "mashable":
+                try:    
+                    soup = BeautifulSoup(value.text, 'xml')
+                except Exception as e:    
+                    print('Could not parse the xml: ', self.url)
+                    print(e)
+
+                articles = soup.findAll('item')
+                articles_dicts = [{'source':'Mashable',
+                'title':a.find('title').text,
+                'content':a.find('description').text,
+                'published':a.find('pubDate').text,
+                'id':a.find('link').text
+                } for a in articles]
+                all_articles.append(articles_dicts)
+            
+            #Finally we merge the list into one
+            output_list = []
+            for list in all_articles:
+                output_list.extend(list)
+
+        return output_list
  
 if __name__ == '__main__':
  
-    feed = ReadRss(headers, url_list = ["techcrunch"])
+    feed = ReadRss(HEADERS, url_list = ["mashable","techcrunch","verge"])
     #feed = feed.parse_articles()
     print(feed.parse_articles())
